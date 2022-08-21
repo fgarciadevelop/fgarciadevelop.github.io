29df52a19c282c22d0a094ac9038e6af44210412
Fran García <fgarciadevelop@gmail.com>
2022-08-21 14:38:23 +0200

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ export class AppComponent {
   title = 'portfolio';
   collapse = true;
 
+  public about = {
+    who: '¡Hola! Soy Fran, graduado en ingeniería informática por la Universidad de Alicante. También trabajo a tiempo completo como desarrollador frontend especialziado en Angular. En mis tiempos libres también desarrollo proyectos por mi cuenta.',
+    why: 'Soy un ingeniero con amplios conocimientos tanto técnicos como funcionales. Cuento con más de 2 años de experiencia laboral como desarrollador de software. Actualmente me encuentro cursando el máster universitario de Ingeniería Informática que me habilita como ingeniero superior en informática.'
+  }
+
 }
